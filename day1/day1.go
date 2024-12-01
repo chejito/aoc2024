ce2753f608cd7ed992bffa8ef909d1aab9d64544
chejito <smndz81@gmail.com>
2024-12-01 21:05:44 +0100

    Removed unnecessary prints
    Signed-off-by: chejito <smndz81@gmail.com>

--- day1/day1.go
+++ day1/day1.go
@@ -11,9 +11,6 @@ var list2 []int
 var distanceList []int
 
 func Run(lines []string) {
-	for _, line := range lines {
-		fmt.Printf("%s\n", line)
-	}
 	fmt.Println(".: Part 1 :.")
 
 	for _, line := range lines {
@@ -24,23 +21,16 @@ func Run(lines []string) {
 
 	sort.Ints(list1)
 	sort.Ints(list2)
-
-	fmt.Println(list1)
-	fmt.Println(list2)
-
 	for i := 0; i < len(list1); i++ {
 		distance := GetDistance(list1[i], list2[i])
 		distanceList = append(distanceList, distance)
 	}
 
-	fmt.Println(distanceList)
-	sum := 0
+	totalDistance := 0
 	for _, num := range distanceList {
-		sum += num
+		totalDistance += num
 	}
-	fmt.Printf("Total distance is %d\n", sum)
-
-	fmt.Println(".: Part 2 :.")
+	fmt.Printf("Total distance is %d\n", totalDistance)
 }
 
 func GetDistance(i1 int, i2 int) int {
